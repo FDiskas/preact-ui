b19c83e6dce4ac091cb5eb57d4954852804b6e41
Vytenis Kuciauskas <projektas@gmail.com>
2020-12-12 18:54:13 +0200

    Setup initial

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,28 +1,28 @@
 import { FunctionalComponent, h } from 'preact';
 import { Route, Router, RouterOnChangeArgs } from 'preact-router';
 
-import Home from '../routes/home';
-import Profile from '../routes/profile';
 import NotFoundPage from '../routes/notfound';
-import Header from './header';
+import { Header } from './header/Header';
+import Home from 'src/routes/home';
+import Profile from 'src/routes/profile';
 
 const App: FunctionalComponent = () => {
-    let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
-    };
+  let currentUrl: string;
+  const handleRoute = (e: RouterOnChangeArgs) => {
+    currentUrl = e.url;
+  };
 
-    return (
-        <div id="app">
-            <Header />
-            <Router onChange={handleRoute}>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
-        </div>
-    );
+  return (
+    <div id="app">
+      <Header />
+      <Router onChange={handleRoute}>
+        <Route path="/" component={Home} />
+        <Route path="/profile/" component={Profile} user="me" />
+        <Route path="/profile/:user" component={Profile} />
+        <NotFoundPage default />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
